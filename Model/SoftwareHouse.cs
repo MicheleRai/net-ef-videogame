aefa8b7a1684d51de8b9b675557051ef0a905f43
MicheleRai <rainonemichele1999@gmail.com>
2023-03-31 15:51:21 +0200

    Add project files.

---
+++ Model/SoftwareHouse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_ef_videogame.Model
+{
+    public class SoftwareHouse
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public long NumeroPIva { get; set; }
+        public string? Citta { get; set; }
+        public string? Nazione { get; set; }
+
+        public IEnumerable<Videogame>? Videogames { get; set; }
+    }
+}
---
+++ Model/Videogame.cs
@@ -0,0 +1,24 @@
+﻿using net_ef_videogame.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_ef_videogame.Model
+{
+    [Table("Videogame")]
+
+    [Index(nameof(Id), IsUnique = true )]
+    public class Videogame
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Overview { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        public int SoftwareHouseId { get; set; }
+        public SoftwareHouse? SoftwareHouse { get; set; }
+    }
+}
